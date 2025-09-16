1797dd3d0eba2fc31ff65c903c11e284833a9e18
Hengji-cs <hengjizhou01@gmail.com>
2025-09-16 21:55:59 +0800

    wan video agent

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import asyncio
 import logging
-import json
-from pipelines.novel2movie_pipeline import Novel2MoviePipeline
-from pipelines.script2video_pipeline import Script2VideoPipeline
-logging.basicConfig(level=logging.INFO)
-logging.info("Pipeline initialized.")
+from PIL import Image
+from tools.video_generator.wan import WanVideoGenerator
 
-pipeline = Script2VideoPipeline.init_from_config(
-    config_path="configs/script2video.yaml",
-    working_dir=".working_dir/script2video",
+logging.basicConfig(level=logging.INFO)
+
+api_key = "9caa641d699a4223b95b7bccebf597c4"
+base_url = "https://yunwu.ai"
+
+save_prefix = "example_inputs/videos/veo_output"
+video_generator = WanVideoGenerator(
+  api_key=api_key,
+  base_url=base_url
 )
 
-
+video = asyncio.run(
+  video_generator.generate_single_video(
+    prompt="A cute magical cat, digital art, watching the stars, running on the beach.",
+    reference_images=[Image.open("example_inputs/images/nanobanana_output/multiple_magical_cat/magical_cat_0.png")],
+  )
+)
+video.save(f"{save_prefix}/single_magical_cat.mp4")
