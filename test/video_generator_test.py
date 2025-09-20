ff4692dd9e38e6ee136c6ef585510c4f4a91086c
tun-sudo <1423993862@qq.com>
2025-09-20 09:36:21 +0800

    添加DoubaoDance视频生成器，更新视频生成测试用例，新增视频输出文件

--- test/video_generator_test.py
+++ test/video_generator_test.py
@@ -29,10 +29,11 @@ video_generator = DoubaoDanceVideoGenerator(
 )
 
 
-prompt = "The transformation process from a seed to a big tree, realistic style, high quality, 4K"
+# prompt = "The transformation process from a seed to a big tree. Natural transition, high detail, vibrant colors."
+prompt = "镜头切换。第一个镜头为一颗种子在土壤中萌发，慢慢长出嫩芽。第二个镜头为大树的树干逐渐长高，树枝伸展。自然过渡，高细节，色彩鲜艳。"
 reference_image_paths = [
     r"example_inputs\images\nanobanana_output\seed.png",
-    # r"example_inputs\images\nanobanana_output\tree.png"
+    r"example_inputs\images\nanobanana_output\tree.png"
 ]
 
 video = asyncio.run(
@@ -42,6 +43,7 @@ video = asyncio.run(
     )
 )
 # save_path = f"{save_prefix}/ff2v.mp4" if len(reference_image_paths) == 1 else f"{save_prefix}/flf2v.mp4"
-save_path = f"{save_prefix}/seed_to_tree_ff2v.mp4"
+# save_path = f"{save_prefix}/seed_to_tree_ff2v.mp4"
+save_path = f"{save_prefix}/seed_to_tree_flf2v.mp4"
 os.makedirs(save_prefix, exist_ok=True)
 video.save(save_path)
